9d96c8990f14331e112db0a25c6aa764d25f1577
Paepcke, Michael <git@paepcke.de>
2023-01-12 06:41:26 +0000

    auto: sync upstream / update dependencies

--- publish.sh
+++ publish.sh
@@ -1,119 +1,134 @@
 #!/bin/sh
-hq u
-BSDlive goo
-BASE="$BSD_DEV/website"
-WWW="$BASE/www"
-INDEX=$WWW/index.html
-LIVE="/.worker/gate/var/squid/reports"
-TARGETPAGES="$BSD_DEV/paepckehh.github.io $BSD_DEV/pages"
-if [ ! -x "$BASE" ]; then exit 1; fi
-UUID="$(uuidgen)"
-mv -f $WWW $WWW.$UUID > /dev/null 2>&1
-rm -rf $WWW.$UUID > /dev/null 2>&1
-mkdir -p $WWW || exit 1
-cp -f $BASE/.template.index.html $INDEX || exit 1
-ls -I $BSD_DEV | while read line; do
-	cd $BSD_DEV/$line > /dev/null 2>&1 || continue
+setup() {
+	hq u
+	BSDlive goo
+	go version || exit 1
+	GOPREPONLY="true" /etc/action/fmt.sh || exit 1
+	GOPREPONLY="true" /etc/action/fmt.golang || exit 1
+	(cd $BSD_DEV/website && sh /etc/action/fmt.sh)
+	if [ "$DIST" == "pnoc" ]; then
+		export HTTPS_PROXY=127.0.0.80:8080
+		export SSL_CERT_FILE=/etc/ssl/rootCA.pem
+		export GOPROXY="proxy.golang.org"
+		export GOSUMDB="sum.golang.org+033de0ae+Ac4zctda0e5eza+HJyk9SxEdh+s3Ux18htTTAD8OuAn8"
+	else
+		export GOPROXY="failfast"
+	fi
+	export BASE="$BSD_DEV/website"
+	export WWW="$BASE/www"
+	export INDEX=$WWW/index.html
+	export LIVE="/.worker/gate/var/squid/reports"
+	export TARGETPAGES="$BSD_DEV/paepckehh.github.io $BSD_DEV/pages"
+	export UUID="$(uuidgen)"
+	export REPOS="$(cd $BSD_DEV && ls -I | grep -v action)"
+	if [ -z "$REPOS" ] || [ ! -x "$BASE" ]; then echo "unable to access repo" && exit 1; fi
+	rm -rf $WWW || exit 1
+	mkdir -p $WWW || exit 1
+	cp -f $BASE/.template.index.html $INDEX || exit 1
+}
+rebuild() {
+	/usr/bin/git prune --expire=now
+	/usr/bin/git reflog expire --expire-unreachable=now --rewrite --all
+	/usr/bin/git pack-refs --all
+	/usr/bin/git repack -n -a -d -f --depth=64 --window=300 --threads=0
+	/usr/bin/git prune-packed
+	/usr/bin/git fsck --strict --unreachable --dangling --full --cache
+}
+clean_push() {
+	if [ -e go.mod ]; then
+		sh /etc/action/fmt.golang || exit 1
+		go version || exit 1
+		rm -rf go.mod go.sum > /dev/null 2>&1
+		go mod init paepcke.de/$REPO
+		case $REPO in
+		gps*) ;;
+		*) sed -i '' -e 's/go 1\.20/go 1\.19/g' go.mod ;;
+		esac
+		GOSUMDB="sum.golang.org+033de0ae+Ac4zctda0e5eza+HJyk9SxEdh+s3Ux18htTTAD8OuAn8" go mod tidy
+	fi
+	/usr/bin/git rm -r --cached .
+	/usr/bin/git add .
+	/usr/bin/git commit -m "auto: sync upstream / update dependencies"
+	/usr/bin/git gc --auto || exit 1
+	# sh /etc/action/git.push
+}
+build_project() {
 	echo "###################################################################################"
-	echo "### [start] [$line]"
-	if [ -e .export ]; then
-		NEW="$WWW/$line"
-		mkdir -p "$NEW" || exit 1
-		cd $NEW || exit 1
-		cp $BASE/.template.project.html index.html || exit 1
-		if [ -e $BSD_DEV/$line/.apionly ]; then
-			sed -i '' -e "/INSTALL/d" index.html
-			sed -i '' -e "/DOWNLOAD/d" index.html
-		fi
-		if [ -e $BSD_DEV/$line/.dataonly ]; then
-			sed -i '' -e "/DOCS/d" index.html
-			sed -i '' -e "/INSTALL/d" index.html
-			sed -i '' -e "/DOWNLOAD/d" index.html
-		fi
-		if [ -e $BSD_DEV/$line/.norelease ]; then
-			sed -i '' -e "/DOWNLOAD/d" index.html
-		fi
-		if [ ! -e $BSD_DEV/$line/.github.com ]; then
-			sed -i '' -e "/github/d" index.html
-		fi
-		if [ ! -e $BSD_DEV/$line/.gitlab.com ]; then
-			sed -i '' -e "/gitlab/d" index.html
-		fi
-		if [ ! -e $BSD_DEV/$line/.codeberg.org ]; then
-			sed -i '' -e "/codeberg/d" index.html
-		fi
-		if [ ! -e $BSD_DEV/$line/.sr.ht ]; then
-			sed -i '' -e "/git.sr.ht/d" index.html
-		fi
-		sed -i '' -e "s/XXXPKGXXX/$line/g" index.html
-		echo "<tr><td><a href="$line"><button>$line</button></a></td></tr>" >> $INDEX
-		# sh /etc/goo/goo.mdtohtml $BSD_DEV/$line/README.md readme.html
-		cd $BSD_DEV/$line > /dev/null 2>&1 || continue
-		if [ -x .git ]; then
-			if [ "$FIXURL" == "true" ] && [ -e README.md ]; then goo.xurls.fix README.md; fi
-			if [ "$DIST" == "pnoc" ]; then
-				if [ -e go.mod ]; then
-					export HTTPS_PROXY=127.0.0.80:8080
-					export SSL_CERT_FILE=/etc/ssl/rootCA.pem
-					export GOPROXY="proxy.golang.org"
-					export GOSUMDB="sum.golang.org+033de0ae+Ac4zctda0e5eza+HJyk9SxEdh+s3Ux18htTTAD8OuAn8"
-					rm -rf go.mod go.sum > /dev/null 2>&1
-					/usr/local/goo/.freebsd.arm/bin/go mod init paepcke.de/$line
-					case $line in
-					gps*) ;;
-					*) sed -i '' -e 's/go 1\.20/go 1\.19/g' go.mod ;;
-					esac
-					/usr/local/goo/.freebsd.arm/bin/go mod tidy
-				fi
-				if [ -e go.mod ]; then
-					sh /etc/action/fmt.golang
-				fi
-				/usr/bin/git rm -r --cached .
-				/usr/bin/git add .
-				/usr/bin/git commit -m "auto: sync upstream / update dependencies"
-				/usr/bin/git gc
-				sh /etc/action/git.push
-			fi
-			if [ "$DIST" == "bsrv" ]; then
-				/usr/bin/git prune --expire=now
-				/usr/bin/git reflog expire --expire-unreachable=now --rewrite --all
-				/usr/bin/git pack-refs --all
-				/usr/bin/git repack -n -a -d -f --depth=64 --window=300 --threads=0
-				/usr/bin/git prune-packed
-				/usr/bin/git fsck --strict --unreachable --dangling --full --cache
-			fi
-			if [ ! -z "$UPSIG" ]; then
-				LATEST=$(doasgit -C $BSD_GIT/.repo/github_com_paepckehh_$line describe --tags --abbrev=0)
-				echo "$line => tag: $LATEST"
-				HQ_ADD_SIGNIFY=true sh /etc/action/git.sign github_com_paepckehh_$line $LATEST
-			fi
-		fi
-		echo "### [done] [$line]"
-		echo "###################################################################################"
+	echo "### [start] [$REPO]"
+	NEW="$WWW/$REPO"
+	mkdir -p "$NEW" || exit 1
+	cd $NEW || exit 1
+	cp $BASE/.template.project.html index.html || exit 1
+	if [ -e $BSD_DEV/$REPO/.apionly ]; then
+		sed -i '' -e "/INSTALL/d" index.html
+		sed -i '' -e "/DOWNLOAD/d" index.html
 	fi
-done
-echo "</table></div><br><br><br><a href="imp.html"><button> [impressum] [contact] </button></a><br></body></html>" >> $INDEX
-cp -f $BASE/.template.imp.html $WWW/imp.html
-cp -f $BASE/.template.keys $WWW/keys/keys
-cp -f $BASE/.template.keys $WWW/paepcke.keys
-chown -R 0:0 $WWW
-chmod -R o=rX,g=rX,u=rX $WWW
-for PAGES in $TARGETPAGES; do
-	if [ -x "$PAGES" ]; then
-		cp -af $WWW/* $PAGES/
-		(
-			cd $PAGES && (
-				/usr/bin/git add .
-				/usr/bin/git commit -m "generate static site"
-				/usr/bin/git gc
-				if [ "$DIST" == "pnoc" ]; then sh /etc/action/git.push; fi
+	if [ -e $BSD_DEV/$REPO/.dataonly ]; then
+		sed -i '' -e "/DOCS/d" index.html
+		sed -i '' -e "/INSTALL/d" index.html
+		sed -i '' -e "/DOWNLOAD/d" index.html
+	fi
+	if [ -e $BSD_DEV/$REPO/.norelease ]; then
+		sed -i '' -e "/DOWNLOAD/d" index.html
+	fi
+	if [ ! -e $BSD_DEV/$REPO/.github.com ]; then
+		sed -i '' -e "/github/d" index.html
+	fi
+	if [ ! -e $BSD_DEV/$REPO/.gitlab.com ]; then
+		sed -i '' -e "/gitlab/d" index.html
+	fi
+	if [ ! -e $BSD_DEV/$REPO/.codeberg.org ]; then
+		sed -i '' -e "/codeberg/d" index.html
+	fi
+	if [ ! -e $BSD_DEV/$REPO/.sr.ht ]; then
+		sed -i '' -e "/git.sr.ht/d" index.html
+	fi
+	sed -i '' -e "s/XXXPKGXXX/$REPO/g" index.html
+	echo "<tr><td><a href="$REPO"><button>$REPO</button></a></td></tr>" >> $INDEX
+	cd $BSD_DEV/$REPO > /dev/null 2>&1 || continue
+	if [ -x .git ]; then
+		/usr/bin/git gc --auto --force || exit 1
+		if [ "$DIST" == "pnoc" ]; then clean_push; fi
+		if [ "$DIST" == "bsrv" ]; then rebuild; fi
+		if [ ! -z "$UPSIG" ]; then
+			LATEST=$(doasgit -C $BSD_GIT/.repo/github_com_paepckehh_$REPO describe --tags --abbrev=0)
+			echo "$REPO => tag: $LATEST"
+			HQ_ADD_SIGNIFY=true sh /etc/action/git.sign github_com_paepckehh_$REPO $LATEST
+		fi
+		if [ "$FIXURL" == "true" ] && [ -e README.md ]; then
+			goo.xurls.fix README.md || exit 1
+		fi
+	fi
+	echo "### [done] [$REPO]"
+	echo "###################################################################################"
+}
+action() {
+	for REPO in $REPOS; do
+		cd $BSD_DEV/$REPO && if [ -e .export ]; then build_project; fi
+	done
+	echo "</table></div><br><br><br><a href="imp.html"><button> [impressum] [contact] </button></a><br></body></html>" >> $INDEX
+	cp -f $BASE/.template.imp.html $WWW/imp.html
+	cp -f $BASE/.template.keys $WWW/keys/keys
+	cp -f $BASE/.template.keys $WWW/paepcke.keys
+	chown -R 0:0 $WWW
+	chmod -R o=rX,g=rX,u=rX $WWW
+	for PAGES in $TARGETPAGES; do
+		if [ -x "$PAGES" ]; then
+			cp -af $WWW/* $PAGES/
+			(
+				cd $PAGES && (
+					if [ "$DIST" == "pnoc" ]; then clean_push; fi
+				)
 			)
-		)
+		fi
+	done
+	if [ -x "$LIVE" ]; then
+		mv -f $LIVE/www $LIVE/www.$UUID
+		cp -af $WWW $LIVE/
+		rm -rf $LIVE/www.$UUID > /dev/null 2>&1
 	fi
-done
-if [ -x "$LIVE" ]; then
-	mv -f $LIVE/www $LIVE/www.$UUID
-	cp -af $WWW $LIVE/
-	rm -rf $LIVE/www.$UUID > /dev/null 2>&1
-fi
-if [ "$DIST" == "bsrv" ]; then cd $GOMODCACHE && fsdd --hard-link .; fi
+	if [ "$DIST" == "bsrv" ]; then cd $GOMODCACHE && fsdd --hard-link .; fi
+}
+setup
+action
+exit 0
